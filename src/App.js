8eab0adfee6cab2f21ff290a9532be6895d32e05
GentleToyin <41758364+GentleToyin@users.noreply.github.com>
2021-06-21 10:54:06 +0100

    Introduction to JS
    Done exercise 1.3-1.5. Implemented array and methods. Part1b

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = (props)=>{
   return(
     <div>
-      <h1>{props.course}</h1>
+      <h1>{props.course.name}</h1>
     </div>
     )
   }
@@ -11,9 +11,9 @@ const Header = (props)=>{
   const Content = (props)=>{
     return(
       <div>
-        <p>{props.part1}</p>
-        <p>{props.part2}</p>
-        <p>{props.part3}</p>
+        <p>{props.course.parts.[0].name}</p>
+        <p>{props.course.parts.[1].name}</p>
+        <p>{props.course.parts.[2].name}</p>
       </div>
       )
   }
@@ -21,26 +21,36 @@ const Header = (props)=>{
   const Total = (props)=>{
     return(
       <div>
-         <p>Number of excercises {props.exc1 + props.exc2 + props.exc3}</p>
+         <p>Number of excercises {props.course.parts.[0].excercises1 + props.course.parts.[1].excercises2 + props.course.parts.[2].excercises3}</p>
       </div>
       )
   }
 
 const App =()=>{
-  const course = 'Half Stack application development'
-  const part1='Fundamentals of React'
-  const excercises1 = 10
-  const part2= 'Using props to pass data'
-  const excercises2 = 7
-  const part3 = 'State of a component'
-  const excercises3 = 14
+  const course = {
+  name: 'Half Stack application development',
+  parts:[
+    {
+       name: 'Fundamentals of React',
+       excercises1 : 10
+    },
+    {
+       name:'Using props to pass data',
+       excercises2 : 7
+  },
+   {
+      name:'State of a component',
+      excercises3 : 14
+  }
+  ]
+  }
+
 
   return (
     <div>
       <Header course = {course}/>
-      <Content part1 = {part1} part2 = {part2} part3 = {part3} />
-      <Total exc1 = {excercises1} exc2={excercises2} exc3= {excercises3}/>
-     
+      <Content  course={course}/>
+      <Total  course={course}/>
     </div>
     )
 }
